ba0aa1babeb16055ddaaec35cf9033bc37fd8f60
Varun Gujjar <varun.gujjar0406@gmail.com>
2026-02-28 09:52:55 +0100

    Frontend moved to ba-frontend repository

--- web/frontend/src/services/search.tsx
+++
@@ -1,9 +0,0 @@
-import { useSocketRequest } from "@/store/useSocketRequest";
-
-export const useSearchService = () => {
-  const { request } = useSocketRequest();
-
-  return {
-    getSearch: (query?: string) => request("search.search", {query}),
-  }
-};
